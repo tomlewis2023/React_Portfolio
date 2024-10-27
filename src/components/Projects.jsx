0400f3a3c86dee7597506fbd873f4b4ff396e406
tomlewis2023 <tomlewis2005@gmail.com>
2024-10-27 09:03:27 +0530

    update 27.10.24

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,19 +6,37 @@ import proj3 from "../assets/proj3.jpg";
 import proj4 from "../assets/proj4.jpg";
 import proj5 from "../assets/proj5.jpg";
 import proj6 from "../assets/proj6.jpg";
+import proj7 from "../assets/proj7.jpg";
+import proj8 from "../assets/proj8.jpg";
 
 
 const projects = [
+
+
   {
-    title: "Project One",
-    description: "Kerala Tourism UI/UX design using HTML,CSS & Bootstrap.",
+    title: "React LMS Frontend",
+    description: "LMS Frontend using React hosted in vercel",
+    img: proj7,
+    link: "https://student-portal-lime.vercel.app/"
+    
+  },
+  {
+    title: "Full Stack MERN",
+    description: "Full Stack MERN Ecommerce, using React, Taiwind, Express, Cloudinary, Stripe, hosted in Vercel",
+    img: proj8,
+    link: "https://fullstack-e-commerce-u1vh.vercel.app/"
+    
+  },
+  {
+    title: "Frontend HTML, CSS",
+    description: "Kerala Tourism Frontend using HTML,CSS & Bootstrap.",
     img: proj1,
     link: "https://tomlewis2023.github.io/keralatourismcss/index.html"
    
   },
   {
-    title: "Project Two",
-    description: "Myntra UI/UX design using HTML,CSS ",
+    title: "Frontend HTML, CSS",
+    description: "Myntra Frontend using HTML,CSS ",
     img: proj2,
     link: "https://tomlewis2023.github.io/MyntraTom/"
       
@@ -26,33 +44,34 @@ const projects = [
     
   },
   {
-    title: "Project Three",
-    description: "Netflix UI/UX design using HTML,CSS",
+    title: "Frontend HTML, CSS",
+    description: "Netflix Frontend using HTML,CSS",
     img: proj3,
     link: "https://tomlewis2023.github.io/NetflixTom/"
     
   },
   {
-    title: "Project Four",
+    title: "Weather APP JS",
     description: "Weather App Worldwide using Javascript ",
     img: proj4,
     link: "https://tomlewis2023.github.io/weatherapp/"
     
   },
   {
-    title: "Project Five",
+    title: "Random User JS",
     description: "Random user Generator using Javascript",
     img: proj5,
     link: "https://tomlewis2023.github.io/FetchApi/"
     
   },
   {
-    title: "Project Six",
-    description: "FoodApp UI/UX design using HTML,CSS,Bootstrap",
+    title: "Frontend HTML , CSS",
+    description: "FoodApp Frontend using HTML,CSS,Bootstrap",
     img: proj6,
     link: "https://tomlewis2023.github.io/FoodcartBootstrap/"
     
   },
+  
 ];
 
 const Projects = () => {
